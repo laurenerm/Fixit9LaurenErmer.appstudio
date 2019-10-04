664a1d81dea5736160c5fbbd23adcbdcaa1d766d
Lauren <laurenermer@hotmail.com>
2019-10-03 21:36:59 -0500

    Initial commit

---
+++ forms/membership/membership.js
@@ -0,0 +1,15 @@
+
+var members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+
+
+btnEnter.onclick=function(){
+let firstName= inptFirstName.value;
+console.log(members.includes(firstName))
+  if (members.includes(firstName) == true) {
+  lblMember.value = `Welcome back, ${firstName}`;
+  }
+else {
+  lblMember.value = `You are not a member, ${firstName}`;
+  }
+  
+}
