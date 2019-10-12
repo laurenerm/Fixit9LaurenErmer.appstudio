400e9ba671a9cb29961af3eece832ab0a1a39f5e
Lauren <laurenermer@hotmail.com>
2019-10-11 21:03:55 -0500

    final

--- forms/fourControls/fourControls.js
+++ forms/fourControls/fourControls.js
@@ -1,9 +1,17 @@
 
 jmbControl.onclick=function(){
-  NSB.MsgBox('Congradulations! Your mouse works\:\)')
-}
-
+  NSB.MsgBox('Congrats! Your mouse works\:\)')
+};
 
 sldControl.onchange=function(){
-  alert('You moved me!')
-}
+  NSB.MsgBox('Wow good job!')
+};
+
+drpdwn.onclick=function(){
+  NSB.MsgBox('I go right instead of left')
+};
+
+
+pgnA.onclick=function(){
+  NSB.MsgBox('Good letter choice')
+};
